1f203e4e54ca65bf48adb394cf9754846fc48837
lodyne <lodgmtui@gmail.com>
2024-08-07 09:31:07 +0300

    modify contract and presenter for GE Profile

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/contract/CoreGeProfileContract.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/contract/CoreGeProfileContract.java
@@ -1,4 +1,51 @@
 package org.smartregister.chw.core.contract;
 
-public class CoreGeProfileContract {
+import org.json.JSONObject;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+
+public interface CoreGeProfileContract {
+
+    public class ClientInfo{
+        public String firstname;
+        public String middlename;
+        public String lastname;
+        public Integer age;
+        public String location;
+        public String gender;
+        public String uniqueId;
+
+        public ClientInfo(String firstname, String middlename, String lastname, Integer age, String location, String gender,String uniqueId) {
+            this.firstname = firstname;
+            this.middlename = middlename;
+            this.lastname = lastname;
+            this.age = age;
+            this.gender=gender;
+            this.location = location;
+            this.uniqueId = uniqueId;
+        }
+
+        public String getFullname(){
+            return firstname+ " " +middlename+ " " +lastname;
+        }
+
+        public String getUniqueId(){
+            return "ID: "+uniqueId;
+        }
+
+    }
+    public interface View{
+        void startFormActivity(JSONObject jsonObject);
+
+    }
+    public interface Presenter{
+
+        void convertDataToEvent(String data);
+
+        ClientInfo getClientsInfo(CommonPersonObjectClient clientsInfo);
+
+    }
+    public interface Model{
+        ClientInfo getClientsInfo(CommonPersonObjectClient clientsInfo);
+    }
+
 }
--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/presenter/CoreGeProfilePresenter.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/presenter/CoreGeProfilePresenter.java
@@ -1,4 +1,25 @@
 package org.smartregister.chw.core.presenter;
 
-public class CoreGeProfilePresenter {
+import android.content.Context;
+
+import org.jetbrains.annotations.NotNull;
+import org.smartregister.chw.core.contract.CoreGeProfileContract;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+
+
+public class CoreGeProfilePresenter implements CoreGeProfileContract.Presenter {
+
+    private String data;
+
+    @Override
+    public void convertDataToEvent(String data) {
+        this.data=data;
+    }
+
+    @Override
+    public CoreGeProfileContract.ClientInfo getClientsInfo(CommonPersonObjectClient clientsInfo) {
+        return null;
+    }
+
+
 }
